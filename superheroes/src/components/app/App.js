690021d0bb96d7ee2506fadfd3257eaa22f681f8
Ekaterina Kotenko <104442215+FrauReise@users.noreply.github.com>
2022-11-22 17:32:46 +0300

    first commit

---
+++ superheroes/src/components/app/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Card from '../Supercard/Supercard.jsx';
+import data from '../../data/data.json';
+
+function App() {
+  return (
+    <div className="App">
+
+      {
+      data.map((hero) =>
+      <Card 
+      name={hero.heroName} 
+      universe={hero.universe} 
+      alterEgo={hero.alterEgo} 
+      occupation={hero.occupation} 
+      friends={hero.friends} 
+      power={hero.power} 
+      url={hero.picture} 
+      details={hero.details}>
+      </Card>
+      )
+    }
+    </div>
+  );
+}
+
+export default App;
